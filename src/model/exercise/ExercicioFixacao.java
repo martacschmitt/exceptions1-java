3972bd98b95150c0e764e13a1ae062c48b6ecddc
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-03-29 22:03:45 -0300

    Exercicio fixacao

---
+++ src/model/exercise/ExercicioFixacao.java
@@ -0,0 +1,48 @@
+package model.exercise;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.AccountException;
+
+public class ExercicioFixacao {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);		
+		Scanner sc = new Scanner(System.in);
+		try {
+			
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			sc.nextLine();
+			
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			
+			Account account = new Account(number, holder, balance, withdrawLimit);
+			
+			System.out.println();
+			
+			System.out.print("Enter amount for withdraw: ");
+			double amount = sc.nextDouble();
+			account.withdraw(amount);
+			
+			System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
+			
+		} catch (AccountException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Unexpected error"); 
+		}
+		
+		sc.close();
+	}	
+}
